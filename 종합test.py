0ef4c13fadd2ce90228764a1545d630539012b27
luckjun0910 <162653711+luckjun0910@users.noreply.github.com>
2024-03-15 11:45:43 +0900

    Add files via upload

---
+++ 종합test.py
@@ -0,0 +1,44 @@
+# #3-1
+# print("Hello world")
+# print("gello  world")
+# print("hello"+"world")
+# print("Hello" + " " + "world")
+
+# print("오늘은 파이썬 챌린지 3일차 입니다")
+# print('디버깅'+'연습중입니다')
+# print('내일은 금요일' + '입니다')
+# print(("그래서 챌린지가 없습니다"))
+
+#3-5
+# a = input ("seokjun")
+# print("me" + a + "yes")
+#print("나는 " + input("당신의 이름은 무엇입니까?") + " 입니다") #터미널에 입력해야 출력이 된다.
+
+
+# name = input("당신의 이름은 무엇입니까?")
+# print(name)
+
+# name = input("당신의 이름은 무엇입니까?")
+# length = len(name)
+# print(length)
+
+#3-6
+# a = input("a:")
+# b = input("b:")
+
+# c = a
+# a = b
+# b = c
+
+# print("a = " + a)
+# print("b = " + b)
+
+#3-7
+# a = "seokjun"
+# print (a) 
+
+# ㄴ
+
+# input ("당신이 좋아하는 영화는 무엇인가요?")
+# input ("주인공은요?")
+print ("내가 좋아하는영화는" + input("당신이 좋아하는 영화는 무엇인가요?") + "이고" + "주인공은" + input("주인공은요?") + "입니다")
