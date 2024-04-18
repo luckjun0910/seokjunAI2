3079f4d6c2a27a6d0fc11d59d578a1df604d6794
luckjun0910 <162653711+luckjun0910@users.noreply.github.com>
2024-04-18 19:05:43 +0900

    Update from googletrans import Translator.py

--- from googletrans import Translator.py
+++ from googletrans import Translator.py
@@ -1,17 +1,13 @@
 from googletrans import Translator
 import qrcode
 
-# 번역할 텍스트
 text_to_translate = "Hello my name is hong"
 
-# 번역
 translator = Translator()
 translated_text = translator.translate(text_to_translate, dest="ko").text
 
-# QR 코드 생성
 img = qrcode.make(translated_text)
 
-# QR 코드 이미지 파일로 저장
-img.save("translated_qr_code.png")
+img.save("번역문장qr코드.png")
 
 print("QR 코드가 생성되었습니다.")
